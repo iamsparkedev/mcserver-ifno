437386a298ac65e2a36c24210c838bdbe0fec60b
iamsparkedev <sifathasansami2@gmail.com>
2025-10-18 16:08:52 +0600

    api intrigation

--- main.go
+++ main.go
@@ -9,15 +9,16 @@ import (
 	"mcserver/handlers"
 )
 
-
 func main() {
-	http.HandleFunc("/api/server", handlers.ServerInfoHandler)//http://localhost:8080/api/server?ip=gate.horizonheaven.top
+	
+	http.HandleFunc("/api/server", handlers.ServerInfoHandler) // http://localhost:8080/api/server?ip=host:port
 	http.HandleFunc("/api/list", handlers.ServerListHandler)
-
+	
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
 	addr := fmt.Sprintf(":%s", port)
 	fmt.Printf("Server is running on port %s\n", port)
 	log.Fatal(http.ListenAndServe(addr, nil))
